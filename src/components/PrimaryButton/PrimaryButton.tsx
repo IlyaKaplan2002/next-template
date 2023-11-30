456461645c8d702740d510bf3f5baab3eec30e62
Ilya Kaplan <ik@alphaguilty.io>
2023-11-30 19:52:25 +0200

    feat(app): add docker

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -1,4 +1,6 @@
+import { spaceGrotesk } from "@/styles/fonts";
 import { ButtonBase } from "@mui/material";
+import classNames from "classnames";
 import { FC, ReactNode } from "react";
 
 interface Props {
@@ -7,7 +9,12 @@ interface Props {
 
 const PrimaryButton: FC<Props> = ({ children }) => {
   return (
-    <ButtonBase className="rounded-btn bg-btn px-4 py-2.5 text-sm/4 font-medium text-white">
+    <ButtonBase
+      className={classNames(
+        spaceGrotesk.className,
+        "rounded-btn bg-btn px-4 py-2.5 text-sm/4 font-medium text-white",
+      )}
+    >
       {children}
     </ButtonBase>
   );
---
+++ src/styles/fonts.ts
@@ -0,0 +1,4 @@
+import { Space_Grotesk, DM_Sans } from "next/font/google";
+
+export const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
+export const dmSans = DM_Sans({ subsets: ["latin"] });
